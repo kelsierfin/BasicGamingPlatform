1a1a83d60cb161d5518e65cf3a1961ed7fd9b6e1
30206918 <nathan.taylor1@ucalgary.ca>
2025-03-14 14:21:57 -0600

    Added documentation and ranked MMR logic to PlayerStats

--- src/Leaderboard_and_Matchmaking_Classes/PlayerStats.java
+++ src/Leaderboard_and_Matchmaking_Classes/PlayerStats.java
@@ -1,15 +1,31 @@
 package Leaderboard_and_Matchmaking_Classes;
 
+// This class is for giving the information from a completed game to the player statistics database
+// TODO: Update the function to reference placeholder functions correctly once they have been created
+// gameType could be changed to a Game object or maybe an int
+// Import player profile once it is created
+
 public class PlayerStats {
 
-    /*
-     * This function references functions that are not yet made and can be changed later to fit the given names
-     * Also gameType could instead be a Game object? maybe just an int as well since String seems just quite unneeded
-     * Also not 100% sure if PlayerStatDatabase needs to be initialized as an object or if I can reference all the time
+    /**
+     * Adds player statistics from a completed game to the database
+     *
+     * @param player1 The profile of the player who went first
+     * @param player2 The profile of the player who went second
+     * @param player1Won Whether or not player1 won the game that was played
+     * @param gameType The type of game played (chess, checkers, etc.)
+     * @param turns How many turns were played until the winning move
+     * @param time The amount of time the match took to complete
      */
     public void updatePlayerStats(Profile player1, Profile player2, boolean player1Won, String gameType, int turns, int time) {
 
-        // This just adds all the player data into the database based on who won the game
+        // Get the mmr from the two players prior to the match, including how far apart they are
+        int mmr1 = player1.getElo();
+        int mmr2 = player2.getElo();
+        int mmrDifference = mmr2 - mmr1;
+        int rankChange = 20;
+
+        // Add a win to the profile of the winning player, a loss to the other
         if (player1Won) {
             PlayerStatDatabase.addWin(player1, gameType, turns, time);
             PlayerStatDatabase.addLoss(player2, gameType, turns, time);
@@ -18,10 +34,40 @@ public class PlayerStats {
             PlayerStatDatabase.addLoss(player1, gameType, turns, time);
         }
 
-        // Logic for ELO/MMR changes based on who won will be done here which will get sent as a player profile to leaderboard
+        // Logic for ELO/MMR changes based on who won will be based on relative ranking differences
         // Example: Winner gets flat rate +20 +-1 for every 10 points opponent is above or below in rank
         // Winner gets minimum of 8, no less. Gain is reversed for the loser but can only lose 6 minimum
+        // There is currently no maximum rank gain or loss, that could be added if necessary (+-40 maximum for example)
+        // There will also be next to no rank inflation, more calculations would be needed to create an "average rank"
+        if (player1Won) {
+            rankChange = 20 + (mmrDifference / 10);
+            if (rankChange < 8) {
+                mmr1 += 8;
+                if (rankChange < 6) {
+                    mmr2 -= 6;
+                } else {
+                    mmr2 -= rankChange;
+                }
+            } else {
+                mmr1 += rankChange;
+                mmr2 -= rankChange;
+            }
+        } else {
+            rankChange = 20 - (mmrDifference / 10);
+            if (rankChange < 8) {
+                mmr2 += 8;
+                if (rankChange < 6) {
+                    mmr1 -= 6;
+                } else {
+                    mmr1 -= rankChange;
+                }
+            } else {
+                mmr2 += rankChange;
+                mmr1 -= rankChange;
+            }
+        }
 
+        // Update the database with the new mmr numbers and send it to update the leaderboard data
         PlayerStatDatabase.updateElo(player1, mmr);
         PlayerStatDatabase.updateElo(player2, mmr);
         Leaderboard.updateLeaderboard(gameType, player1);
