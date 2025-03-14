37ad45d31bd26d00381f4a61b6ef3798f939aee6
Zihang Huang <zihang.huang@ucalgary.ca>
2025-03-13 23:18:56 -0600

    Update 2 files
    - /Game Logic Team Files/GoGameBasicClass/Board.java
    - /Game Logic Team Files/GoGameBasicClass/Player.java

--- Game Logic Team Files/GoGameBasicClass/Player.java
+++ Game Logic Team Files/GoGameBasicClass/Player.java
@@ -1,37 +1,43 @@
 public class Player {
-    //0 = unset; 1 = black; 2 = white;
+    // 0 = unset; 1 = black; 2 = white;
     int team = 0;
-    //Initialize UCID as 0, each player use UCID to login.
-    //Remember to let user register by their UCID.
+    // Initialize UCID as 0, each player use UCID to login.
+    // Remember to let user register by their UCID.
     int UCID;
-    //Initialize empty password
+    // Initialize empty password
     String password = "";
-    public Player(int team, int UCID, String password){
+
+    public Player(int team, int UCID, String password) {
         this.team = team;
         this.UCID = UCID;
         this.password = password;
-    }
-    //Setters and Getters
-    private void setTeam(int team){
-        this.team = team;
-    }
-    private void setUCID(int UCID){
-        this.UCID = UCID;
-    }
-    private void setPassword(String password){
-        this.password = password;
-    }
-    private int getTeam(){
-        return this.team;
-    }
-    private int getUCID(){
-        return this.UCID;
-    }
-    private String getPassword(){
-        return this.password;
-    }
-    public void placeStone(int x, int y){
-        //Place the stone to the x, y position
     }
 
-}
+    // Setters and Getters
+    public void setTeam(int team) {
+        this.team = team;
+    }
+
+    public void setUCID(int UCID) {
+        this.UCID = UCID;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getTeam() {
+        return this.team;
+    }
+
+    public int getUCID() {
+        return this.UCID;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+
+    public void placeStone(int x, int y, Board board) {
+        board.placeStone(x, y, this.team);
+    }
