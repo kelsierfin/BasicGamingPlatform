5c20d9b554c35d2adbce635baa4016adeeabec3d
Min Oh 30099454 <min.oh@ucalgary.ca>
2025-03-21 23:23:44 -0600

    Rough code of both matchdata and matchmakinglogic. Will work on these in the future. Code in txt atm

--- src/Leaderboard_and_Matchmaking_Classes/MatchData.java
+++ src/Leaderboard_and_Matchmaking_Classes/MatchData.java
@@ -1,4 +1,24 @@
-package Leaderboard_and_Matchmaking_Classes;
-
-public class MatchData {
-}
+//package Leaderboard_and_Matchmaking_Classes;
+//
+//// Rough code where the purpose is to get the code from the GameStats and store it to use might be reduce to redundancy in the future
+//import java.util.*;
+//
+//public class MatchData {
+//    private List<GameStats> players;
+//
+//    public MatchData() {
+//        this.players = new ArrayList<>();
+//    }
+//
+//    public void addPlayer(GameStats player) {
+//        players.add(player);
+//    }
+//
+//    public List<GameStats> getPlayers() {
+//        return players;
+//    }
+//
+//    public String getPlayerStats(GameStats player) {
+//        return player.getELO()
+//    }
+//}
