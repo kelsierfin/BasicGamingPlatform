64b9913960601b62b12ff3e19d2e145db2dccd04
30097393 <egheosasere.ebose@ucalgary.ca>
2025-03-29 03:06:32 -0600

    Navigation bar - notification alert functionality added

--- src/main/java/ca/ucalgary/seng/p3/HomeController.java
+++ src/main/java/ca/ucalgary/seng/p3/HomeController.java
@@ -5,7 +5,13 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
+import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+
+import javax.swing.text.Style;
+import java.util.ArrayList;
 import java.util.List;
 public class HomeController {
 
@@ -13,6 +19,18 @@ public class HomeController {
     private ScrollPane scrollPane;
     @FXML
     private VBox menuPopup = new VBox();
+    @FXML
+    private VBox profilePopup = new VBox();
+    @FXML
+    private VBox notificationPopup = new VBox();
+
+    @FXML
+    private Button profileButton, bellButton;
+
+    @FXML
+    private Circle notificationDot;
+
+    private List<String> notifications = new ArrayList<>();
 
     @FXML
     private Button btnPrev, btnNext;
@@ -48,6 +66,10 @@ public class HomeController {
         btnPrev.setOnAction(event -> scrollLeft());
         btnNext.setOnAction(event -> scrollRight());
         menuPopup.setVisible(false);
+        profilePopup.setVisible(false);
+        notificationPopup.setVisible(false);
+        notifications.add("Welcome user!");
+        notifications.add("Match invite from: ...");
 
         List<ImageView> icons = List.of(chessIcon, goIcon, tttIcon, connectIcon);
 
@@ -60,6 +82,8 @@ public class HomeController {
 
             icon.setClip(rect);
         }
+
+        updateNotificationDot();
     }
 
     private void scrollLeft() {
@@ -77,12 +101,62 @@ public class HomeController {
     private void handleMenuButton() {
         // Toggle visibility of the popup
         menuPopup.setVisible(!menuPopup.isVisible());
+        if(notificationPopup.isVisible()) {
+            notificationPopup.setVisible(false);
+        }
+        if(profilePopup.isVisible()) {
+            profilePopup.setVisible(false);
+        }
     }
 
     // More event handlers for other buttons, unfinished:
     @FXML
     private void handleProfileButton() {
+        // Toggle visibility of the popup
+        profilePopup.setVisible(!profilePopup.isVisible());
+        if(notificationPopup.isVisible()) {
+            notificationPopup.setVisible(false);
+        }
+        if(menuPopup.isVisible()) {
+            menuPopup.setVisible(false);
+        }
     }
+
+    @FXML
+    private void handleBellButton() {
+        // Toggle visibility of the popup
+        updateNotificationDot();
+
+        for(String notification: notifications) {
+            Button btn = new Button(notification);
+            btn.getStyleClass().add("notification-button");
+            notificationPopup.getChildren().add(btn);
+        }
+
+        notificationPopup.setVisible(!notificationPopup.isVisible());
+        if (profilePopup.isVisible()) {
+            profilePopup.setVisible(false);
+        }
+        if (menuPopup.isVisible()) {
+            menuPopup.setVisible(false);
+        }
+
+        notifications = new ArrayList<>();
+
+    }
+
+    private void updateNotificationDot() {
+        notificationDot.setVisible(!notifications.isEmpty());
+    }
+
+    @FXML
+    private void handleEditProfileButton() {
+    }
+
+    @FXML
+    private void handleLogOutButton() {
+    }
+
 
     @FXML
     private void handleDashboardButton() {
@@ -102,9 +176,7 @@ public class HomeController {
     @FXML
     private void handleGameRulesButton() {
     }
-    @FXML
-    private void handleBellButton() {
-    }
+
 
     @FXML
     private void handlePlayChessButton() {
