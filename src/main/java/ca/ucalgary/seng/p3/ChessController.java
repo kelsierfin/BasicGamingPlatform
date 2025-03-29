f1673305488e36af5ae5f855d8f3a22124d3d2bd
30111861 <kemuel.david@ucalgary.ca>
2025-03-29 11:14:58 -0600

    Added a new folder for game assets. Added some code for a chat

--- src/main/java/ca/ucalgary/seng/p3/ChessController.java
+++ src/main/java/ca/ucalgary/seng/p3/ChessController.java
@@ -1,4 +1,26 @@
 package ca.ucalgary.seng.p3;
 
+import javafx.fxml.FXML;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.KeyEvent;
+
 public class ChessController {
+    @FXML
+    private TextField chat_Type_Field;
+
+    @FXML
+    private Label opponent_Name;
+
+    @FXML
+    private ImageView opponent_Profile_pic;
+
+    @FXML
+    void input_Into_Chat(KeyEvent event) {
+
+    }
+
 }
