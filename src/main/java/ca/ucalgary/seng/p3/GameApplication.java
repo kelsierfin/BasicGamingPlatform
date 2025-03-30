ecdbecadd0790eeff8a6599fea80c8e9b6defc1d
30209973 <mdtibyanbin.alam@ucalgary.ca>
2025-03-30 00:09:09 -0600

    testings some files

--- src/main/java/ca/ucalgary/seng/p3/GameApplication.java
+++ src/main/java/ca/ucalgary/seng/p3/GameApplication.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class GameApplication extends Application{
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("/player_Finder.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GameApplication.class.getResource("/settings.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
 
         stage.setTitle("Placeholder Title");
