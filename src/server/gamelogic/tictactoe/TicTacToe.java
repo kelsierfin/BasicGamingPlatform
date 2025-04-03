71f055b9b06b49444f2748cd3588b6549329a6ce
30227964 <sabyasachi.barua@ucalgary.ca>
2025-04-03 11:19:03 -0600

    Added TicTacToe for running the game. (Test purposes)

---
+++ src/server/gamelogic/tictactoe/TicTacToe.java
@@ -0,0 +1,14 @@
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class TicTacToe {
+    private static Input input;
+    private static Print print;
+
+    public static void main(String[] args) {
+        print = new Print(new PrintStream(System.out));
+        input = new Input(new Scanner(System.in), print);
+        GameRunner runner = new GameRunner(input, print);
+        runner.run();
+    }
+}
